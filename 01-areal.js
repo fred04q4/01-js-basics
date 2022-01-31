5e6364e81a9a05c7d31dc98f5d14ef9d9960500f
Frederik Heller <93513730+fred04q4@users.noreply.github.com>
2022-01-31 12:30:59 +0100

    øvelse 2

--- 01-areal.js
+++ 01-areal.js
@@ -1,6 +1,6 @@
 let lændge = 3;
 let bredde = 5;
 let areal = lændge * bredde;
-let resultat ="Længden er 3 meter og bredden er 5 meter. Arealet er lændge*bredde";
+let resultat =`Længden er ${lændge} meter og bredden er ${bredde} meter. Arealet er${areal} `;
 
 console.log(resultat);
---
+++ 02-areal.js
@@ -0,0 +1,20 @@
+let lændge = 3;
+let bredde = 5;
+let areal = lændge * bredde;
+let resultat;
+
+if(areal < 100){
+    resultat ="Arealet er for lille";
+
+}else if (areal < 200) {
+resultat ="Arealet er ok";
+}else{
+    resultat ="Arealet er for stort";   
+}
+
+
+
+
+console.log(resultat);
+
+
